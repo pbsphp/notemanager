164d89122ce3a51b3ead48d8373ee9cffc5b3507
pbsphp <pbsphp@yandex.ru>
2015-08-15 00:38:51 +0300

    Now notes truly belongs to users. @login_required added

--- notemanager/notes/views.py
+++ notemanager/notes/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 from .models import Note, NoteCategory
 
 
+@login_required
 def index(request):
     return JsonResponse({
-        "notes": list(Note.objects.values(
+        "notes": list(Note.objects.filter(user=request.user)
+        .values(
             "id",
             "title",
             "created_at",
@@ -19,6 +22,7 @@ def index(request):
     })
 
 
+@login_required
 def categories(request):
     return JsonResponse({
         "categories": list(NoteCategory.objects.values(
@@ -28,6 +32,7 @@ def categories(request):
     })
 
 
+@login_required
 def create(request):
     data = request.POST
     n = Note()
@@ -37,17 +42,18 @@ def create(request):
 
     category_id = data.get("category__id", None)
     n.category = NoteCategory.objects.get(id=category_id)
-    n.user = User.objects.get(id=1)
+    n.user = request.user
     n.save()
     return HttpResponse("")
 
 
+@login_required
 def update(request):
     data = request.POST
     id_ = data["id"]
-    n = Note.objects.get(id=id_)
+    n = Note.objects.filter(user=request.user).get(id=id_)
     n.title = data.get("title", n.title)
-    n.is_favorite = data.get("is_favorite", n.is_favorite)
+    n.is_favorite = data.get("is_favorite", False)
     n.text = data.get("text", n.text)
 
     category_id = data.get("category__id", n.category_id)
@@ -56,7 +62,8 @@ def update(request):
     return HttpResponse("")
 
 
+@login_required
 def destroy(request):
     id_ = request.POST["id"]
-    Note.objects.get(id=id_).delete()
+    Note.objects.filter(user=request.user).get(id=id_).delete()
     return HttpResponse("")
