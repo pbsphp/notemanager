ceaa952415539d21c0161e8a61cc46158a070c18
pbsphp <pbsphp@yandex.ru>
2015-08-14 17:15:11 +0300

    /notes/ responds with json containing all notes (all fields)

--- notemanager/notemanager/urls.py
+++ notemanager/notemanager/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^profiles/', include('profiles.urls')),
+    url(r'^notes/', include('notes.urls')),
 )
--- notemanager/notes/views.py
+++ notemanager/notes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Note, NoteCategory
+
+
+def index(request):
+    return JsonResponse({
+        "notes": list(Note.objects.values(
+            "id",
+            "title",
+            "created_at",
+            "category__id",
+            "category__name",
+            "is_favorite",
+            "text"
+        ))
+    })
