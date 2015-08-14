b1a01392eeff8523f266fc5f9fa85be43ee8e697
pbsphp <pbsphp@yandex.ru>
2015-08-14 23:38:39 +0300

    Backend support for create/update actions. Quickfix in templates: send id field on update, not on create

--- notemanager/notes/views.py
+++ notemanager/notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.models import User
 
 from .models import Note, NoteCategory
 
@@ -28,8 +29,28 @@ def categories(request):
 
 
 def create(request):
-    pass
+    data = request.POST
+    n = Note()
+    n.title = data.get("title", None)
+    n.is_favorite = data.get("is_favorite", False)
+    n.text = data.get("text", None)
+
+    category_id = data.get("category__id", None)
+    n.category = NoteCategory.objects.get(id=category_id)
+    n.user = User.objects.get(id=1)
+    n.save()
+    return HttpResponse("")
 
 
 def update(request):
-    pass
+    data = request.POST
+    id_ = data["id"]
+    n = Note.objects.get(id=id_)
+    n.title = data.get("title", n.title)
+    n.is_favorite = data.get("is_favorite", n.is_favorite)
+    n.text = data.get("text", n.text)
+
+    category_id = data.get("category__id", n.category_id)
+    n.category = NoteCategory.objects.get(id=category_id)
+    n.save()
+    return HttpResponse("")
