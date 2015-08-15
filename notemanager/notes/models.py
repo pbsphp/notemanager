3e20f44b820de6ff937d910248ddc228e443b03e
pbsphp <pbsphp@yandex.ru>
2015-08-15 23:56:03 +0300

    Note and NoteCategory has been registered in admin.py

--- notemanager/notes/models.py
+++ notemanager/notes/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
 
 
+@python_2_unicode_compatible
 class NoteCategory(models.Model):
     name = models.CharField(unique=True, max_length=254)
 
+    def __str__(self):
+        return "<NoteCategory: %s>" % self.name
 
+
+@python_2_unicode_compatible
 class Note(models.Model):
     class Meta:
         ordering = ["-created_at"]
@@ -18,3 +24,6 @@ class Note(models.Model):
     is_favorite = models.BooleanField(default=False)
     text = models.TextField(blank=True)
     uuid = models.CharField(max_length=64, null=True, unique=True)
+
+    def __str__(self):
+        return "<Note #%d %.16s>"% (self.id, self.title)
