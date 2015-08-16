e606ebddb48d09d227eccf3384aa830823d3afc6
pbsphp <pbsphp@yandex.ru>
2015-08-16 21:50:58 +0300

    WhiteNoise for serving static files on heroku

--- notemanager/notemanager/settings.py
+++ notemanager/notemanager/settings.py
@@ -90,4 +90,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
